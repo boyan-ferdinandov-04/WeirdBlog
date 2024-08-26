431b3b15e779a046fd5b2286c3a45bb90276801d
Boyan Ferdinandov <chopperbobber963@gmail.com>
2024-08-26 17:51:44 +0300

    Add project files.

---
+++ WeirdBlog.DataAccess/Data/WeirdBlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeirdBlog.Models;
+
+namespace WeirdBlog.DataAccess.Data
+{
+    public class WeirdBlogDbContext : DbContext
+    {
+        public WeirdBlogDbContext(DbContextOptions<WeirdBlogDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ WeirdBlog.Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WeirdBlog.Utility;
+
+namespace WeirdBlog.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(StaticConstants.CategoryNameMaxLength)]
+        public string Name { get; set; }
+    }
+}
