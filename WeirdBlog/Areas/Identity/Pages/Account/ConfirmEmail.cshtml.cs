41b065dbe42218fd2ac25e328a3b939cdb48ba40
Boyan Ferdinandov <chopperbobber963@gmail.com>
2025-02-22 22:16:39 +0200

    Registration Changes

--- WeirdBlog/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ WeirdBlog/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,35 +1,28 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
-using System.Linq;
+﻿using System;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using WeirdBlog.Models;
+using WeirdBlog.Utility; 
 
 namespace WeirdBlog.Areas.Identity.Pages.Account
 {
     public class ConfirmEmailModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public ConfirmEmailModel(UserManager<ApplicationUser> userManager)
+        public ConfirmEmailModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
         public string StatusMessage { get; set; }
+
         public async Task<IActionResult> OnGetAsync(string userId, string code)
         {
             if (userId == null || code == null)
@@ -38,16 +31,26 @@ namespace WeirdBlog.Areas.Identity.Pages.Account
             }
 
             var user = await _userManager.FindByIdAsync(userId);
-            
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{userId}'.");
             }
 
-            var decodedCode = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ConfirmEmailAsync(user, decodedCode);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
-            return Page();
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (!result.Succeeded)
+            {
+                StatusMessage = "Error confirming your email.";
+                return Page();
+            }
+
+            if (!await _userManager.IsInRoleAsync(user, StaticConstants.Role_User))
+            {
+                await _userManager.AddToRoleAsync(user, StaticConstants.Role_User);
+            }
+
+            StatusMessage = "Thank you for confirming your email.";
+            return RedirectToPage("./Login");
         }
     }
 }
